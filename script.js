5ae53fadd60abba07967e7bdd606d8649d7f6654
Magnus Roland Raadla <magnus.roland.raadla@gmail.com>
2023-09-25 11:28:01 +0300

    tunnitöö

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
-
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
-
-let x = 10;
-let y = 10;
-let pressed = false;
-let arcSize = 20;
-setInterval(() => {
-    if(pressed){
-        ctx.beginPath();
-        ctx.arc(x,y,arcSize,0,2 * Math.PI);
-        ctx.fill();   
-    } 
-}, 16);
-
-canvas.addEventListener('mousemove', event => {
-    console.log(event);
-    x = event.clientX - canvas.offsetLeft;
-    y = event.clientY - canvas.offsetTop;
-});
-canvas.addEventListener('mousedown', event => {
-    pressed = true;
-});
-canvas.addEventListener('mouseup', event => {
-    pressed = false;
-});
-let size = document.querySelector('#size');
-let sizeValue = document.querySelector('#sizeValue');
-sizeValue.innerHTML = size.value;
-size.addEventListener('input', ()=> {
-    sizeValue.innerHTML = size.value;
-    arcSize = size.value;
-})
+// Flag
+ctx.rect(20,15,50,20);
+ctx.fillStyle = 'blue';
+ctx.fillRect(20,15,50,20);
+ctx.rect(20,35,50,20);
+ctx.fillStyle = 'black';
+ctx.fillRect(20,35,50,35);
+ctx.rect(20,55,50,20);
+ctx.fillStyle = 'white';
+ctx.fillRect(20,55,50,20);
+ctx.stroke();
+// Star
+ctx.beginPath(); 
+ctx.moveTo(100,55);
+ctx.lineTo(120,70);
+ctx.lineTo(110,50);
+ctx.lineTo(100,70);
+ctx.lineTo(120,55);
+ctx.lineTo(100,55);
+ctx.stroke();
+// Pie
+ctx.beginPath();
+ctx.arc(110,100, 20, 0, 2 * Math.PI);
+ctx.lineWidth = 1;
+ctx.fillStyle = 'black'; //this will make sense later
+ctx.fill();
+ctx.stroke();
+// Slice
+ctx.beginPath();
+ctx.lineWidth = 1;
+ctx.arc(110,100, 20, 5, 10);
+ctx.lineTo(110,100);
+ctx.lineTo(110,80);
+ctx.fillStyle = 'white'; //goes on top of the *first* one, so it looks all the same
+ctx.fill();
+ctx.stroke();
